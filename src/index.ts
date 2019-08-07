74484209f59f926c7d87ccd9a1168fdecf98ea25
Monday Victor A <vkingmaker1@gmail.com>
2019-08-07 22:59:12 +0100

    typescript interfaces-generic collection sorting

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import { Sorter } from './Sorter';
+import { NumbersCollection } from './NumbersCollection';
+
+const numbersCollection = new NumbersCollection([50, 3, -5, 0]);
+const sorter = new Sorter(numbersCollection);
+
+sorter.sort();
+
+console.log(sorter.collection);
