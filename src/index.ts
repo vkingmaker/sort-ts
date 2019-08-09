78240ccb6e2e0fdcca2f8bc01206d7443e2af506
Monday Victor A <vkingmaker1@gmail.com>
2019-08-09 10:15:48 +0100

    feature: sort
    This commit sorts array of numbers, a string or a linkedlist
    
    Delivers [#sort]

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,21 @@
 import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
+import { CharactersCollection } from './CharactersCollection';
+import { LinkedList } from './LinkedList';
 
 const numbersCollection = new NumbersCollection([50, 3, -5, 0]);
-const sorter = new Sorter(numbersCollection);
+numbersCollection.sort();
+console.log(numbersCollection.data);
 
-sorter.sort();
+const charactersCollection = new CharactersCollection('Xaayb');
+charactersCollection.sort();
+console.log(charactersCollection.data);
 
-console.log(sorter.collection);
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(-3);
+linkedList.add(4);
+
+linkedList.sort();
+linkedList.print();
